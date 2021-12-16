bf6c19927f4ed52bf4b5e51991c309982c873a9b
silky <silkysantosh09@gmail.com>
2021-12-16 09:54:58 +0100

    add reusable commands

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
+#imp commands
 import time
 
 from selenium import webdriver
 #install pip install webdriver-manager in the terminal
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-time.sleep(5)
+driver.implicitly_wait(10)
+driver.maximize_window()
+
+
+time.sleep(10)
+#driver.minimize_window()
+
 driver.quit()
-print("Testing done!!")
+print("Testing done and refreshed instead of submit the form!!👏👏")
 
 
 
