85ec48fc911b012233b013fb70051bb8f89c435c
silky <silkysantosh09@gmail.com>
2021-12-20 10:31:56 +0100

    [DP-5]add some functionality

--- OrangeHrmLive/orangehrm_live.py
+++ OrangeHrmLive/orangehrm_live.py
@@ -23,10 +23,28 @@ print("Title of the page: ", driver.title)
 driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
 driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
 driver.find_element(By.ID, 'btnLogin').click()
+
+#Click Admin
+driver.find_element(By.XPATH, '//*[@id="menu_admin_viewAdminModule"]').click()
+
+#click username multi Checkboxes
+driver.find_element(By.ID, 'ohrmList_chkSelectRecord_46').click()
+driver.find_element(By.ID, 'ohrmList_chkSelectRecord_39').click()
+driver.find_element(By.ID, 'ohrmList_chkSelectRecord_38').click()
+driver.find_element(By.ID, 'ohrmList_chkSelectRecord_33').click()
+driver.find_element(By.XPATH, '//*[@id="ohrmList_chkSelectRecord_23"]').click()
+
+
+#PIM
+driver.find_element(By.XPATH, '//*[@id="mainMenuFirstLevelUnorderedList"]/li[2]').click()
+driver.find_element(By.XPATH, '//*[@id="ohrmList_chkSelectRecord_64"]').click()
+
+
+#time.sleep(5)
 #Logout
 driver.find_element(By.ID, 'welcome').click()
-driver.find_element(By.XPATH, '//*[@id="welcome-menu"]/ul/li[3]').click()
+driver.find_element(By.XPATH, '//*[@id="mainMenuFirstLevelUnorderedList"]/li[1]').click()
 
-time.sleep(5)
+#time.sleep(5)
 driver.quit()
 print("Testing done!")
