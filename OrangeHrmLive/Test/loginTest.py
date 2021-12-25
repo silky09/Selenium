6e33de2acc532c82ed0d744ff9ae86499b9d991d
silky <silkysantosh09@gmail.com>
2021-12-25 18:33:48 +0100

    add unit testing

--- OrangeHrmLive/Test/loginTest.py
+++ OrangeHrmLive/Test/loginTest.py
@@ -12,33 +12,31 @@ from selenium.webdriver.support.select import Select
 class LoginTest(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls) -> None:
+    def setUpClass(cls):
         # get the page through url
         cls.driver = webdriver.Chrome(ChromeDriverManager().install())
         # maximize window
         cls.driver.maximize_window()
         cls.driver.implicitly_wait(10)
 
-
-
-
     def test_login_validity(self):
         # Login panel
-        self.driver.get("https://opensource-demo.orangehrmlive.com/")
-        self.driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
-        self.driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
-        self.driver.find_element(By.ID, 'btnLogin').click()
+        driver = self.driver
+        driver.get("https://opensource-demo.orangehrmlive.com/")
+        driver.find_element(By.ID, 'txtUsername').send_keys("Admin")
+        driver.find_element(By.ID, 'txtPassword').send_keys("admin123")
+        driver.find_element(By.ID, 'btnLogin').click()
 
         # Click Admin
-        self.driver.find_element(By.XPATH, '//*[@id="menu_admin_viewAdminModule"]').click()
 
+        self.driver.find_element(By.XPATH, '//*[@id="menu_admin_viewAdminModule"]').click()
         # click username multi Checkboxes
         self.driver.find_element(By.ID, 'ohrmList_chkSelectRecord_46').click()
         self.driver.find_element(By.ID, 'ohrmList_chkSelectRecord_39').click()
         self.driver.find_element(By.ID, 'ohrmList_chkSelectRecord_38').click()
         self.driver.find_element(By.ID, 'ohrmList_chkSelectRecord_33').click()
         self.driver.find_element(By.XPATH, '//*[@id="ohrmList_chkSelectRecord_23"]').click()
-        # time.sleep()
+
 
         # PIM
         self.driver.find_element(By.XPATH, '//*[@id="mainMenuFirstLevelUnorderedList"]/li[2]').click()
@@ -49,13 +47,14 @@ class LoginTest(unittest.TestCase):
         # driver.find_element(By.XPATH, '//*[@id="ohrmList_chkSelectRecord_54"]').click()
 
         # time.sleep(3)
+
         # Logout
         self.driver.find_element(By.ID, 'welcome').click()
         time.sleep(3)
         self.driver.find_element(By.XPATH, '//*[@id="mainMenuFirstLevelUnorderedList"]/li[1]').click()
 
     @classmethod
-    def tearDownClass(cls) -> None:
+    def tearDownClass(cls):
         # time.sleep(5)
         cls.driver.quit()
 
@@ -64,9 +63,12 @@ class LoginTest(unittest.TestCase):
         print("Testing done!")
 
 
+if __name__ == '__main__':
+    unittest.main()
 
-
-
+# to run in the terminal
+# cd C:\Users\santo\PycharmProjects\SeleniumDemoProject\OrangeHrmLive\Test
+# python -m unittest loginTest.py (filename)
 
 
 
